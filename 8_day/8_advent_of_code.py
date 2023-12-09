745bbb7c023767bd03942242d95c46c8c86a79a7
Andrea Montes <andrea.montes@yello.co>
2023-12-08 21:51:17 -0500

    part 2

--- 8_day/8_advent_of_code.py
+++ 8_day/8_advent_of_code.py
@@ -1,5 +1,6 @@
 RIGHT = 'R'
 LEFT = 'L'
+from math import lcm
 
 def get_next_instruction(instructions: list):
 	number_instructions = len(instructions)
@@ -35,45 +36,58 @@ def get_mapping(line: str):
 
 def part1(mappings: dict, instructions: list):
 	steps = get_number_steps(mappings, instructions, 'AAA')
-	#print (f'instructions {instructions}')
-	#print (f'mappings {mappings}')
 	print(f'Part 1 steps {steps}')
 
-
-
-def does_ends_with_Z(node: str):
+def does_end_with_Z(node: str):
 	return True if node.endswith('Z') else False
 
-def valid_end_nodes(end_nodes: list):
-	list_validation_ends_with_Z = list(map(does_ends_with_Z, end_nodes))
-	return False if False in list_validation_ends_with_Z else True
-
-
-def get_number_steps_part2(mappings: dict, instructions: list):
+def get_steps_return_to_z(node: str, gen_instruction):
 	steps = 0
 	gen_instruction = get_next_instruction(instructions)
-	transit_nodes = [key for key in mappings if key.endswith('A')]
-	print(f'transit_nodes {transit_nodes}')
-	while not valid_end_nodes(transit_nodes):
+	transit_node = node
+	while True:
 		instruction = next(gen_instruction)
-		print (f'transit node {transit_nodes} - steps {steps} - instruction {instruction}')
+		#print (f'transit node {transit_node} - steps {steps} - instruction {instruction}')
 		if instruction == RIGHT:
-			transit_nodes = [mappings[transit_node][1] for transit_node in transit_nodes]
+			transit_node = mappings[transit_node][1]
 		elif instruction == LEFT:
-			transit_nodes = [mappings[transit_node][0] for transit_node in transit_nodes]
+			transit_node = mappings[transit_node][0]
 		else:
 			print('You are doing something wrong')
 			break
 		steps +=1
-	return steps
+		if does_end_with_Z(transit_node):
+			break
+	return transit_node, steps
 
+def get_number_steps_part2(mappings: dict, instructions: list):
+	transit_nodes = [key for key in mappings if key.endswith('A')]
+	steps_by_transit_nodes = []
+	print(f'transit_nodes {transit_nodes}')
+
+	for transit_node in transit_nodes:
+		steps = 0
+		steps_z_to_z = 0
+		gen_instruction = get_next_instruction(instructions)
+		transit_node, base_steps = get_steps_return_to_z(transit_node, gen_instruction)
+
+		while True:
+			new_transit_node, new_steps_z_to_z = get_steps_return_to_z(transit_node, gen_instruction)
+			steps += new_steps_z_to_z
+			if steps_z_to_z == new_steps_z_to_z:
+				break
+			steps_z_to_z = new_steps_z_to_z
+		steps_by_transit_nodes.append(new_steps_z_to_z)
+	return steps_by_transit_nodes
 
 
 def part2(mappings, instructions):
 	steps = get_number_steps_part2(mappings, instructions)
 	#print (f'instructions {instructions}')
 	#print (f'mappings {mappings}')
+	calculated_steps = lcm(*steps)
 	print(f'Part 2 steps {steps}')
+	print(f'Part 2 calculated_steps  {calculated_steps}')
 
 if __name__ == '__main__':
 	file = open("8_day_input.txt", 'r')
@@ -84,6 +98,6 @@ if __name__ == '__main__':
 		file.readline()
 		map_lines = file.readlines()
 		mappings = dict(map(get_mapping, map_lines))
-		part1(mappings, instructions)
+		#part1(mappings, instructions)
 		part2(mappings, instructions)
 		
